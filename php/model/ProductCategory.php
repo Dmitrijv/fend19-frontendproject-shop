80317a07672e35b1bb565b95bf8d560575289a67
Dmitrijv <dmitrij.vel@live.se>
2020-04-07 22:31:29 +0200

    added an api endpoint for product categories

---
+++ php/model/ProductCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+class ProductCategory {
+
+    private $id;
+    private $name;
+
+    public function __construct( $id, $name ){
+        $this -> id = $id;
+        $this -> name = $name;
+    }
+
+    public function getId(){
+        return $this -> id;
+    }
+
+    public function getName(){
+        return $this -> name;
+    }
+    
+    public function toKeyValueArray(){
+        return array(
+            "id" => $this -> id,
+            "name" => $this -> name,
+        );
+    }
+}
