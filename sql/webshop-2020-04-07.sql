994323ccd9f25c5ee7416d4723924c0fe95e72bf
Dmitrijv <dmitrij.vel@live.se>
2020-04-07 22:01:23 +0200

    added sql dump for the second revision of the db

---
+++ sql/webshop-2020-04-07.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 07, 2020 at 10:00 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `currency`
+--
+
+CREATE TABLE `currency` (
+  `id` char(12) NOT NULL,
+  `shorthand` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `currency`
+--
+
+INSERT INTO `currency` (`id`, `shorthand`) VALUES
+('SEK', 'kr'),
+('USD', '$');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_of_product`
+--
+
+CREATE TABLE `image_of_product` (
+  `product_id` int(11) NOT NULL,
+  `file_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `image_of_product`
+--
+
+INSERT INTO `image_of_product` (`product_id`, `file_name`) VALUES
+(1, '5301791.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordered_product`
+--
+
+CREATE TABLE `ordered_product` (
+  `product_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `currency_id` char(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_of_products`
+--
+
+CREATE TABLE `order_of_products` (
+  `id` int(11) NOT NULL,
+  `date_ordered_at` datetime NOT NULL,
+  `first_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `email` text NOT NULL,
+  `phone` text NOT NULL,
+  `deliveryadress` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `price_of_product`
+--
+
+CREATE TABLE `price_of_product` (
+  `product_id` int(11) NOT NULL,
+  `currency_id` char(12) NOT NULL,
+  `amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `price_of_product`
+--
+
+INSERT INTO `price_of_product` (`product_id`, `currency_id`, `amount`) VALUES
+(1, 'SEK', 3590);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `category_id` int(11) NOT NULL DEFAULT 1,
+  `number_in_stock` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `title`, `description`, `category_id`, `number_in_stock`) VALUES
+(1, 'AMD Ryzen 7 3700X 3.6 GHz 36MB', 'Med snabbare hastigheter, mer minne och bandbredd än den tidigare generationen sätter den tredje generationens Ryzen-processorer från AMD standarden för hög prestanda.', 2, 999);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_category`
+--
+
+CREATE TABLE `product_category` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_category`
+--
+
+INSERT INTO `product_category` (`id`, `name`) VALUES
+(1, 'Miscellaneous'),
+(2, 'CPU');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `currency`
+--
+ALTER TABLE `currency`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `image_of_product`
+--
+ALTER TABLE `image_of_product`
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `ordered_product`
+--
+ALTER TABLE `ordered_product`
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `currency` (`currency_id`);
+
+--
+-- Indexes for table `order_of_products`
+--
+ALTER TABLE `order_of_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `price_of_product`
+--
+ALTER TABLE `price_of_product`
+  ADD KEY `product` (`product_id`),
+  ADD KEY `currency` (`currency_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `product_category`
+--
+ALTER TABLE `product_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order_of_products`
+--
+ALTER TABLE `order_of_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `product_category`
+--
+ALTER TABLE `product_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `image_of_product`
+--
+ALTER TABLE `image_of_product`
+  ADD CONSTRAINT `productkeyy` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ordered_product`
+--
+ALTER TABLE `ordered_product`
+  ADD CONSTRAINT `currencykey` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderkey` FOREIGN KEY (`order_id`) REFERENCES `order_of_products` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `productkey` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `price_of_product`
+--
+ALTER TABLE `price_of_product`
+  ADD CONSTRAINT `currency_key` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_key` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `categconstr` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
