ade3841954395ba43150e7d66b10798537980a9d
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-04-09 13:17:45 +0200

    rough grid layout to be continued
    first version

---
+++ admin/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Area | Paintings</title>
+    <!-- fontawesome CDN -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/adminpanel.css">
+
+</head>
+
+<body>
+    <!-- header begins -->
+    <header class="admin-header">
+        <h1 class="admin-header__title"><i class="fas fa-cogs"></i>Admin panel</h1>
+    </header>
+    <!-- header ends -->
+
+    <!-- main area begins, including sidebar and content area -->
+    <main class="admin-main">
+        <!-- sidebar begins -->
+        <div class="admin-main__sidebar">
+            <ul class="admin-main__sidebar--ul">
+                <li><i class="fas fa-home"></i>HOME</li>
+                <li><i class="fas fa-folder-open"></i>CATEGORY</li>
+                <li><i class="fas fa-images"></i>PRODUCTS</li>
+                <li><i class="fas fa-file"></i>ORDERS</li>
+            </ul>
+        </div>
+        <!-- sidebar ends -->
+
+        <!-- content area begins -->
+        <div class="admin-main__content">
+
+        </div>
+        <!-- content area ends -->
+    </main>
+    <!-- main area ends -->
+</body>
+
+</html>
