c02fd15001a70b9c288f5bdd4c60c498ee26af44
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-04-09 22:59:44 +0200

    edited edit-btn & del-btn on category-page

--- php/model/cms.php
+++ php/model/cms.php
@@ -14,37 +14,36 @@ class CMS
         return self::$instance;
     }
     public static function createAdminPanelCategoryPage()
-    { {
-            $stmt = DB::run("SELECT * FROM product_category ORDER BY id ASC");
-            //create table head
-            $html = '
+    {
+        $stmt = DB::run("SELECT * FROM product_category ORDER BY id ASC");
+        //create table head
+        $html = '
             <table class="admin-main__content__category-table">
             <tr>
                 <th>CATEGORY\'S NAME</th>
                 <th><i class="fas fa-ellipsis-h"></i></th>
             </tr>
             ';
-            // append table rows
-            while ($category = $stmt->fetch(PDO::FETCH_LAZY)) {
-                $html .= "
+        // append table rows
+        while ($category = $stmt->fetch(PDO::FETCH_LAZY)) {
+            $html .= "
                     <tr data-post-id='{$category['id']}'>
                         <td>{$category['name']}</td>
                         <td>
-                        <form style='display: inline-block;' action='edit.php' method='POST' >
-                            <input class='btn btn-default' type='submit' name='edit' value='Edit'>
-                            <input type='hidden' name='postId' value='{$category['id']}'>
+                        <form style='display: inline-block;' action='editCategory.php' method='POST' >
+                            <input class='btn edit-btn' type='submit' name='edit' value='Edit'>
+                            <input type='hidden' name='categoryId' value='{$category['id']}'>
                         </form>
-                        <form style='display: inline-block;' onsubmit='cmsLib.deleteBlogPost(event);'>
-                            <input class='btn btn-danger' data-post-id='{$category['id']}' type='submit' name='delete' value='Delete'>
+                        <form style='display: inline-block;' onsubmit='cmsLib.deleteCategory(event);'>
+                            <input class='btn del-btn' data-post-id='{$category['id']}' type='submit' name='delete' value='Delete'>
                         </form>
                         </td>
                     </tr>";
-            }
-            // close table tag
-            $html .= "
+        }
+        // close table tag
+        $html .= "
             </table>
             ";
-            echo $html;
-        }
+        echo $html;
     }
 }
