dddf2269f803f22632b241cb6949340ce88015c1
g-holmer <gabbeholmer@hotmail.com>
2020-04-09 11:32:20 +0200

    added header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<header class="main__header">
+<div class="main__header__left">
+<span class="hamburger__bar-wrapper">
+<span class="hamburger__bar"></span>
+<span class="hamburger__bar"></span>
+<span class="hamburger__bar"></span>
+</span>
+</div>
+<div class="main__header__right">
+<div class="logo">LOGOTYPE</div>
+<div class="main__header__login">LOGIN</div>
+<div class="main__header__cart">CART</div>
+<div class="main__header__search">SEARCH</div>
+</div>
+<div class="div"></div>
+</header>
