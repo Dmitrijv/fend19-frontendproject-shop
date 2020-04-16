9be9b12c193b32136da3e8244136fc1058a5bff4
g-holmer <gabbeholmer@hotmail.com>
2020-04-16 15:57:54 +0200

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -15,7 +15,13 @@ while ($tableRow = $stmt->fetch(PDO::FETCH_LAZY)) {
 ?>
 
 <div class="sidebar">
-    <nav>
+<input type="radio" id="male" name="gender" value="male">
+<label for="male">Male</label><br>
+<input type="radio" id="female" name="gender" value="female">
+<label for="female">Female</label><br>
+<input type="radio" id="other" name="gender" value="other">
+<label for="other">Other</label>     
+<nav>
         <ul class="sidebar__menu">
         <?php echo $sidebarItems; ?>
         </ul>
