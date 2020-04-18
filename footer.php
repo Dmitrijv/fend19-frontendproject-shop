520a03c85c2b900cca970a8ae0039ba610abda6b
Dmitrijv <dmitrij.vel@live.se>
2020-04-18 14:28:05 +0200

    fixed footer icons

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <footer class="footer">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2034.2614397675409!2d18.021236615935116!3d59.34528361678554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d9d263b022d%3A0x82fc0f30ed84f9ed!2sNackademin!5e0!3m2!1ssv!2sse!4v1586423786552!5m2!1ssv!2sse" 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2034.2614397675409!2d18.021236615935116!3d59.34528361678554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d9d263b022d%3A0x82fc0f30ed84f9ed!2sNackademin!5e0!3m2!1ssv!2sse!4v1586423786552!5m2!1ssv!2sse"
             class="footer__embed__map">
             </iframe>
             <div class="footer__bottom__wrapper">
             <div class="logo"><img class="logo" src="img/svg/Logo.svg" alt="Logo Image"></div>
                 <div class="footer__bottom__wraper__info__wraper">
-                    <h5 class="footer__info__heading">Mer info</h5>
+                    <h5 class="footer__info__heading">Lorem</h5>
                     <ul class="footer__list">
                         <a href="" class="footer__list__categori"><li>Kategori</li></a>
                         <a href="" class="footer__list__categori"><li>Kategori</li></a>
@@ -14,7 +14,7 @@
                     </ul>
                 </div>
                 <div class="footer__bottom__wraper__contact__wraper">
-                    <h5 class="footer__contact__list__heading">Kontakt</h5>
+                    <h5 class="footer__contact__list__heading">Ipsum</h5>
                     <ul class="footer__list">
                         <a href="" class="footer__list__categori"><li>Kategori</li></a>
                         <a href="" class="footer__list__categori"><li>Kategori</li></a>
@@ -24,11 +24,11 @@
                 </div>
                 <div class="footer__icons__wrapper">
                     <h6 class="footer__icon__heading">Följ oss</h6>
-                    <img src="img/icons/instagram.png" alt="" srcset="" class="footer__icons">
-                    <img src="img/icons/instagram.png" alt="" srcset="" class="footer__icons">
-                    <img src="img/icons/instagram.png" alt="" srcset="" class="footer__icons">
-                    <img src="img/icons/instagram.png" alt="" srcset="" class="footer__icons">
-                    <a href=""><button class="footer__contact__button" >Kontakta oss</button></a>
+                    <img src="img/svg/footer/facebook.svg" alt="facebook" class="footer__icons">
+                    <img src="img/svg/footer/instagram.svg" alt="instagram" class="footer__icons">
+                    <img src="img/svg/footer/twitter.svg" alt="twitter" class="footer__icons">
+                    <img src="img/svg/footer/youtube.svg" alt="youtube" class="footer__icons">
+                    <button class="footer__contact__button" >Kontakta oss</button>
                 </div>
             </div>
         </footer>
