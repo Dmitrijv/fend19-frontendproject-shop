81f0d68a51b1e89dff59b4a662f3bbaaa911b21c
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-04-18 17:07:11 +0200

    fixed footer, again

--- footer.php
+++ footer.php
@@ -1,36 +1,66 @@
 <footer class="footer">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2034.2614397675409!2d18.021236615935116!3d59.34528361678554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d9d263b022d%3A0x82fc0f30ed84f9ed!2sNackademin!5e0!3m2!1ssv!2sse!4v1586423786552!5m2!1ssv!2sse"
-            class="footer__embed__map">
-            </iframe>
-            <div class="footer__bottom__wrapper">
-            <div class="logo"><img class="logo" src="img/svg/Logo.svg" alt="Logo Image"></div>
-                <div class="footer__bottom__wraper__info__wraper">
-                    <h5 class="footer__info__heading">Lorem</h5>
-                    <ul class="footer__list">
-                        <a href="" class="footer__list__categori"><li>Kategori</li></a>
-                        <a href="" class="footer__list__categori"><li>Kategori</li></a>
-                        <a href="" class="footer__list__categori"><li>Kategori</li></a>
-                        <a href="" class="footer__list__categori"><li>Kategori</li></a>
-                    </ul>
-                </div>
-                <div class="footer__bottom__wraper__contact__wraper">
-                    <h5 class="footer__info__heading">Ipsum</h5>
-                    <ul class="footer__list">
-                        <a href="" class="footer__list__categori"><li>Kategori</li></a>
-                        <a href="" class="footer__list__categori"><li>Kategori</li></a>
-                        <a href="" class="footer__list__categori"><li>Kategori</li></a>
-                        <a href="" class="footer__list__categori"><li>Kategori</li></a>
-                    </ul>
-                </div>
-                <div class="footer__icons__wrapper">
-                    <h6 class="footer__icon__heading">Följ oss</h6>
-                    <img src="img/svg/footer/facebook.svg" alt="facebook" class="footer__icons">
-                    <img src="img/svg/footer/instagram.svg" alt="instagram" class="footer__icons">
-                    <img src="img/svg/footer/twitter.svg" alt="twitter" class="footer__icons">
-                    <img src="img/svg/footer/youtube.svg" alt="youtube" class="footer__icons">
-                    <button class="footer__contact__button" >Kontakta oss</button>
-                </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2034.2614397675409!2d18.021236615935116!3d59.34528361678554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d9d263b022d%3A0x82fc0f30ed84f9ed!2sNackademin!5e0!3m2!1ssv!2sse!4v1586423786552!5m2!1ssv!2sse" class="footer__embed__map">
+    </iframe>
+    <div class="footer__bottom__wrapper">
+        <div class="logo"><img class="logo" src="img/svg/Logo.svg" alt="Logo Image"></div>
+        <div class="footer__bottom__wrapper__info__wrapper">
+            <h5 class="footer__info__heading">Lorem</h5>
+            <ul class="footer__list">
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+            </ul>
+        </div>
+        <div class="footer__bottom__wrapper__contact__wrapper">
+            <h5 class="footer__info__heading">Ipsum</h5>
+            <ul class="footer__list">
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+                <a href="" class="footer__list__categori">
+                    <li>Kategori</li>
+                </a>
+            </ul>
+        </div>
+        <div class="footer__wrapper">
+            <h6 class="footer__icon__heading">Följ oss</h6>
+            <div class="icon-wrapper">
+                <img src="img/svg/footer/facebook.svg" alt="facebook" class="footer__icons">
+                <img src="img/svg/footer/instagram.svg" alt="instagram" class="footer__icons">
+                <img src="img/svg/footer/twitter.svg" alt="twitter" class="footer__icons">
+                <img src="img/svg/footer/youtube.svg" alt="youtube" class="footer__icons">
             </div>
-        </footer>
-    </body>
+            <button class="footer__contact__button">Kontakta oss</button>
+        </div>
+    </div>
+</footer>
+</body>
+
 </html>
