1cbdf9c95bb620530eab98f9deef557c9d392624
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-04-21 19:46:53 +0200

    new shopLib for IE

--- js/ie11/shopLib.js
+++ js/ie11/shopLib.js
@@ -1,4 +1,4 @@
-shopLib = (function() {
+shopLib = function () {
   var info = "Helper library for drawing html elements based on db data.";
   var version = "0.2";
   var SHOP_URL = "".concat(location.protocol, "//").concat(location.host, "/fend19-frontendproject-shop");
@@ -14,17 +14,14 @@ shopLib = (function() {
       sidebar.innerHTML = "";
       dropdown.innerHTML = ""; // get category json from api
 
-      lib.loadJsonByXhr(categoryApiUrl, function(categoryJson) {
+      lib.loadJsonByXhr(categoryApiUrl, function (categoryJson) {
         // add a default row to the dropdown menu that shows products of all categories
-        var defaultRow =
-          "\n        <li class='sidebar__menu__list-item'>\n            <input class=\"categoryFilterButton\" type='button' id='-1' value='Visa Alla' onclick=\"shopLib.drawFilteredProductPanel(event)\" >\n        </li>";
+        var defaultRow = "\n        <li class='sidebar__menu__list-item'>\n            <input class=\"categoryFilterButton\" type='button' id='-1' value='Visa Alla' onclick=\"shopLib.drawFilteredProductPanel(event)\" >\n        </li>";
         dropdown.innerHTML += defaultRow;
         sidebar.innerHTML += defaultRow; // iterate over all categories
 
-        categoryJson.forEach(function(category) {
-          var categoryRow = "\n            <li class='sidebar__menu__list-item'>\n                <input class=\"categoryFilterButton\" type='button' id='"
-            .concat(category.id, "' value='")
-            .concat(category.name, '\' onclick="shopLib.drawFilteredProductPanel(event)">\n            </li>');
+        categoryJson.forEach(function (category) {
+          var categoryRow = "\n            <li class='sidebar__menu__list-item'>\n                <input class=\"categoryFilterButton\" type='button' id='".concat(category.id, "' value='").concat(category.name, "' onclick=\"shopLib.drawFilteredProductPanel(event)\">\n            </li>");
           dropdown.innerHTML += categoryRow;
           sidebar.innerHTML += categoryRow;
         });
@@ -33,7 +30,7 @@ shopLib = (function() {
     drawDefaultProductPanel: function drawDefaultProductPanel(event) {
       var lib = this;
       var productApi = "".concat(INTERNAL_API_PATH, "/products.php");
-      lib.loadJsonByXhr(productApi, function(productJson) {
+      lib.loadJsonByXhr(productApi, function (productJson) {
         lib.drawProductPanel(productJson);
       });
     },
@@ -41,11 +38,11 @@ shopLib = (function() {
       var lib = this;
       var allowedCategoryId = Number(event.currentTarget.id);
       var productApi = "".concat(INTERNAL_API_PATH, "/products.php");
-      lib.loadJsonByXhr(productApi, function(productJson) {
+      lib.loadJsonByXhr(productApi, function (productJson) {
         if (allowedCategoryId === -1) {
           lib.drawProductPanel(productJson);
         } else {
-          productJson = productJson.filter(function(product) {
+          productJson = productJson.filter(function (product) {
             return product.categoryId === allowedCategoryId;
           });
           lib.drawProductPanel(productJson);
@@ -57,28 +54,9 @@ shopLib = (function() {
     drawProductPanel: function drawProductPanel(productJson) {
       var productPanel = document.querySelector("div#productPanel");
       var cardHtml = "";
-      productJson.forEach(function(item) {
-        var coverImage =
-          item.imageGallery.length > 0 ? "./img/product/" + item.imageGallery[0] : "./img/product/placeholder.png";
-        cardHtml += "\n        <div id='"
-          .concat(
-            item.id,
-            "' class='product grid-box'>\n            <div class='product__img-wrapper grid-3'>\n                <img class='product__img' src='"
-          )
-          .concat(
-            coverImage,
-            "' alt='product name'>\n            </div>\n            <div class='grid-2'>\n                <p class='product__title'>"
-          )
-          .concat(item.title, "</p>\n                <div class='product__price'>")
-          .concat(item.price, " ")
-          .concat(
-            item.currency,
-            "</div>\n                <div class='product__count-container'>\n                    <button class='product__count-btn'>-</button>\n                    <p class='product__count'>"
-          )
-          .concat(
-            item.numberInStock,
-            "</p>\n                    <button class='product__count-btn'>+</button>\n                </div>\n                <button class='product__add-btn ctrl-standard typ-subhed fx-bubbleUp'>L\xE4gg i varukorgen</button>\n            </div>\n        </div>"
-          );
+      productJson.forEach(function (item) {
+        var coverImage = item.imageGallery.length > 0 ? "./img/product/" + item.imageGallery[0] : "./img/product/placeholder.png";
+        cardHtml += "\n        <div id='".concat(item.id, "' class='product grid-box'>\n            <div class='product__img-wrapper grid-3' style=\"background-image: url(").concat(coverImage, ")\">\n            </div>\n            <div class='grid-2'>\n                <p class='product__title'>").concat(item.title, "</p>\n                <div class='product__price'>").concat(item.price, " ").concat(item.currency, "</div>\n                <div class='product__count-container'>\n                    <button class='product__count-btn'>-</button>\n                    <p class='product__count'>").concat(item.numberInStock, "</p>\n                    <button class='product__count-btn'>+</button>\n                </div>\n                <button class='product__add-btn ctrl-standard typ-subhed fx-bubbleUp'>L\xE4gg i varukorgen</button>\n            </div>\n        </div>");
       });
       productPanel.innerHTML = "";
       productPanel.innerHTML += cardHtml; // show error message if this category has no products
@@ -90,6 +68,7 @@ shopLib = (function() {
       } else {
         errorMsg.classList.add("hidden");
       } // add event listeners to "add to cart" buttons
+
 
       var productBtn = document.querySelectorAll(".product__add-btn");
       addProduct(productBtn);
@@ -107,6 +86,7 @@ shopLib = (function() {
       } // validate keyword length again
       // show error message if this keyword is invalid
 
+
       var keywordErrMsg = document.querySelector(".invalidKeywordMessage");
 
       if (keyword.length < 2) {
@@ -119,12 +99,9 @@ shopLib = (function() {
 
       var lib = this;
       var productApi = "".concat(INTERNAL_API_PATH, "/products.php");
-      lib.loadJsonByXhr(productApi, function(productJson) {
-        var matchingProducts = productJson.filter(function(product) {
-          return (
-            product.title.toLowerCase().indexOf(keyword) !== -1 ||
-            product.description.toLowerCase().indexOf(keyword) !== -1
-          );
+      lib.loadJsonByXhr(productApi, function (productJson) {
+        var matchingProducts = productJson.filter(function (product) {
+          return product.title.toLowerCase().indexOf(keyword) !== -1 || product.description.toLowerCase().indexOf(keyword) !== -1;
         }); // console.log(matchingProducts);
 
         lib.drawSearchResultList(matchingProducts);
@@ -148,12 +125,9 @@ shopLib = (function() {
       }
 
       var productApi = "".concat(INTERNAL_API_PATH, "/products.php");
-      lib.loadJsonByXhr(productApi, function(productJson) {
-        var matchingProducts = productJson.filter(function(product) {
-          return (
-            product.title.toLowerCase().indexOf(keyword) !== -1 ||
-            product.description.toLowerCase().indexOf(keyword) !== -1
-          );
+      lib.loadJsonByXhr(productApi, function (productJson) {
+        var matchingProducts = productJson.filter(function (product) {
+          return product.title.toLowerCase().indexOf(keyword) !== -1 || product.description.toLowerCase().indexOf(keyword) !== -1;
         }); // console.log(matchingProducts);
 
         lib.drawSearchResultList(matchingProducts);
@@ -163,21 +137,9 @@ shopLib = (function() {
     drawSearchResultList: function drawSearchResultList(productJson) {
       var productPanel = document.querySelector("div.searchResults");
       var cardHtml = "";
-      productJson.forEach(function(item) {
-        var coverImage =
-          item.imageGallery.length > 0 ? "./img/product/" + item.imageGallery[0] : "./img/product/placeholder.png";
-        cardHtml += "\n        <div id='"
-          .concat(
-            item.id,
-            "' class='product grid-box'>\n            <div class='product__img-wrapper grid-3'>\n                <img class='product__img' src='"
-          )
-          .concat(
-            coverImage,
-            "' alt='product name'>\n            </div>\n            <div class='grid-2'>\n                <p class='product__title'>"
-          )
-          .concat(item.title, "</p>\n                <div class='product__price'>")
-          .concat(item.price, " ")
-          .concat(item.currency, "</div>\n            </div>\n        </div>");
+      productJson.forEach(function (item) {
+        var coverImage = item.imageGallery.length > 0 ? "./img/product/" + item.imageGallery[0] : "./img/product/placeholder.png";
+        cardHtml += "\n        <div id='".concat(item.id, "' class='product grid-box'>\n            <div class='product__img-wrapper grid-3'>\n                <img class='product__img' src='").concat(coverImage, "' alt='product name'>\n            </div>\n            <div class='grid-2'>\n                <p class='product__title'>").concat(item.title, "</p>\n                <div class='product__price'>").concat(item.price, " ").concat(item.currency, "</div>\n            </div>\n        </div>");
       });
       productPanel.innerHTML = "";
       productPanel.innerHTML += cardHtml; // show error message if this category has no products
@@ -193,7 +155,7 @@ shopLib = (function() {
     loadJsonByXhr: function loadJsonByXhr(url, callback) {
       var xhr = new XMLHttpRequest();
 
-      xhr.onreadystatechange = function() {
+      xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
           callback(JSON.parse(this.responseText));
         }
@@ -212,4 +174,4 @@ shopLib = (function() {
     }
   };
   return shopLib;
-})();
+}();
