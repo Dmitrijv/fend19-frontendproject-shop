225a3df0a5609321b80627f810a3be93c303f189
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-04-23 09:53:30 +0200

    refactor

--- js/ie11/autoplay.js
+++ js/ie11/autoplay.js
@@ -1,54 +1,63 @@
-var oBody = document.getElementsByTagName("body")[0];
-var aBanner = document.getElementsByClassName("banner");
-var aSpan = document.getElementsByClassName("tab")[0].getElementsByTagName("span");
-var oNext = document.getElementsByClassName("next")[0];
-var Oprev = document.getElementsByClassName("prev")[0];
-var Oon = document.getElementsByClassName("on")[0];
-aBanner[0].style.opacity = "1";
-aSpan[0].className = "on";
+var freezeArea = document.querySelector(".p-grid-1");
+var banners = document.querySelectorAll(".banner");
+var spans = document.querySelector(".tab").querySelectorAll("span");
+var prevArrow = document.querySelector(".prev");
+var nextArrow = document.querySelector(".next");
+var activeDot = document.querySelector(".on");
+var bannersLength = banners.length;
+
+// initial setting, display first image and first dot
+// if there are more than one image, let's auto-play
+if (bannersLength > 1) {
+  banners[0].style.opacity = "1";
+  spans[0].className = "on";
+} else {// clearInerval
+  spans[0].style.display = "none";
+  prevArrow.style.display = "none";
+  nextArrow.style.display = "none";
+  clearInterval(timer);
+}
 
 var num = 0;
-for (var i = 0; i < aSpan.length; i++) {
-  aSpan[i].index = i;
-  aSpan[i].onclick = function () {
-    for (var j = 0; j < aSpan.length; j++) {
+for (var i = 0; i < bannersLength; i++) {
+  spans[i].index = i;
+  spans[i].onclick = function () { //click dot to display relevant img
+    for (var j = 0; j < bannersLength; j++) {
       num = this.index;
-      aSpan[j].className = "";
-      aBanner[j].style.opacity = "0";
+      spans[j].className = "";
+      banners[j].style.opacity = "0";
     }
-    aSpan[num].className = "on";
-    aBanner[num].style.opacity = "1";
+    spans[num].className = "on";
+    banners[num].style.opacity = "1";
   }
-  oNext.onclick = function () {
-    for (var j = 0; j < aSpan.length; j++) {
-      if (aSpan[j].className == "on") {
-        aSpan[j].className = "";
-        aBanner[j].style.opacity = "0";
+  prevArrow.onclick = function () { //click to show next img
+    for (var j = 0; j < bannersLength; j++) {
+      if (spans[j].className == "on") {
+        spans[j].className = "";
+        banners[j].style.opacity = "0";
         j++;
         num++;
-        if (j > 4) {
+        if (j > bannersLength -1) {
           j = 0;
         }
-        aSpan[j].className = "on";
-        aBanner[j].style.opacity = "1";
-
+        spans[j].className = "on";
+        banners[j].style.opacity = "1";
       }
     }
   }
 
-  Oprev.onclick = function () {
-    for (var j = 0; j < aSpan.length; j++) {
-      if (aSpan[j].className == "on") {
-        aSpan[j].className = "";
-        aBanner[j].style.opacity = "0";
+  prevArrow.onclick = function () { //click to show previous img
+    for (var j = 0; j < bannersLength; j++) {
+      if (spans[j].className == "on") {
+        spans[j].className = "";
+        banners[j].style.opacity = "0";
         j--;
         num--;
         if (j < 0) {
-          j = 4;
+          j = bannersLength -1;
         }
-        aSpan[j].className = "on";
-        aBanner[j].style.opacity = "1";
-
+        spans[j].className = "on";
+        banners[j].style.opacity = "1";
       }
     }
   }
@@ -56,24 +65,24 @@ for (var i = 0; i < aSpan.length; i++) {
 
 function Time() {
   num++;
-  if (num < 5) {
-    for (var j = 0; j < aSpan.length; j++) {
-      aSpan[j].className = "";
-      aBanner[j].style.opacity = "0";
+  if (num < bannersLength) {
+    for (var j = 0; j < bannersLength; j++) {
+      spans[j].className = "";
+      banners[j].style.opacity = "0";
     }
-    aSpan[num].className = "on";
-    aBanner[num].style.opacity = "1";
+    spans[num].className = "on";
+    banners[num].style.opacity = "1";
   } else {
     num = -1;
   }
 }
 clearInterval(timer);
-var timer = setInterval("Time()", 2000);
+var timer = setInterval("Time()", 3000); 
 
-oBody.onmouseover = function () {
+freezeArea.onmouseover = function () {
   clearInterval(timer);
 };
-oBody.onmouseout = function () {
+freezeArea.onmouseout = function () {
   clearInterval(timer);
-  timer = setInterval("Time()", 2000);
+  timer = setInterval("Time()", 3000);
 };
