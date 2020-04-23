4a85c68996d1516abcc25330d9f97e3c812c6b68
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-04-24 00:07:04 +0200

    added some js code for cart effect

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
         <span class="hamburger__bar"></span>
     </span>
 
-    <?php require_once __DIR__ . '/php/view/sidebar.php';?>
-    <?php require_once __DIR__ . '/php/view/header.php';?>
-    <?php require_once __DIR__ . '/php/view/cart.php';?>
+    <?php require_once __DIR__ . '/php/view/sidebar.php'; ?>
+    <?php require_once __DIR__ . '/php/view/header.php'; ?>
+    <?php require_once __DIR__ . '/php/view/cart.php'; ?>
 
     <main>
         <div class="content">
@@ -29,18 +29,46 @@
         </div>
     </main>
 
-    <?php require_once __DIR__ . '/php/view/footer.php';?>
+    <?php require_once __DIR__ . '/php/view/footer.php'; ?>
 
     <script src="./js/ie11/sidebar.js"></script>
     <!-- <script src="./js/sidebar.js"></script> -->
 
     <script type="text/javascript" src="./js/ie11/shopLib.js"></script>
     <!-- <script type="text/javascript" src="./js/shopLib.js"></script> -->
-    <script type="text/javascript"> shopLib.drawCategorySelectors(); </script>
-    <script type="text/javascript"> shopLib.drawDefaultProductPanel(); </script>
+    <script type="text/javascript">
+        shopLib.drawCategorySelectors();
+    </script>
+    <script type="text/javascript">
+        shopLib.drawDefaultProductPanel();
+    </script>
     <!-- <script type="text/javascript" src="./js/ie11/cart.js"></script> -->
     <script type="text/javascript" src="./js/cart.js"></script>
+    <script>
+        var body = document.body,
+            overlay = document.querySelector('.overlay'),
+            overlayBtts = document.querySelectorAll('button[class$="overlay"]');
 
+        [].forEach.call(overlayBtts, function(btt) {
+
+            btt.addEventListener('click', function() {
+
+                /* Detect the button class name */
+                var overlayOpen = this.className === 'open-overlay';
+
+                /* Toggle the aria-hidden state on the overlay and the 
+                   no-scroll class on the body */
+                overlay.setAttribute('aria-hidden', !overlayOpen);
+                body.classList.toggle('noscroll', overlayOpen);
+
+                /* On some mobile browser when the overlay was previously
+                   opened and scrolled, if you open it again it doesn't 
+                   reset its scrollTop property */
+                overlay.scrollTop = 0;
+
+            }, false);
+        });
+    </script>
 </body>
 
 </html>
