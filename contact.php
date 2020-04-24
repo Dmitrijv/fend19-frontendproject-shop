07c9c2a235338f1ff9458a4d64da979256003f84
Dmitrijv <dmitrij.vel@live.se>
2020-04-24 11:04:24 +0200

    added header back to contact form

--- contact.php
+++ contact.php
@@ -1,7 +1,4 @@
-<?php 
-include __DIR__ . '/php/view/contactform.php';
-include __DIR__ . '/php/model/phpmailer.php';
-?>
+<?php include __DIR__ . '/php/view/contactform.php';?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,20 +20,19 @@ include __DIR__ . '/php/model/phpmailer.php';
     </span>
 
     <?php require_once __DIR__ . '/php/view/sidebar.php';?>
-    <?php
-    //  require_once __DIR__ . '/php/view/header.php';?>
+    <?php require_once __DIR__ . '/php/view/header.php';?>
     <?php require_once __DIR__ . '/php/view/cart.php';?>
 
     <main>
         <div class="content_contact">
         <form action="phpmailer.php" method="POST" class="contact__form">
             <h1 class="contact__form__heading">Kontakta oss</h1>
-            <span class="error"><?php echo $nameError; ?></span>            
-            <input type="text" class="contact__form__name" name="name" placeholder="Namn" value="<?= $name ?>">
+            <span class="error"><?php echo $nameError; ?></span>
+            <input type="text" class="contact__form__name" name="name" placeholder="Namn" value="<?=$name;?>">
             <span class="error"><?php echo $emailError; ?></span>
-            <input type="text" class="contact__form__email" name="email" placeholder="E-post adress" value="<?= $email ?>">
+            <input type="text" class="contact__form__email" name="email" placeholder="E-post adress" value="<?=$email;?>">
             <span class="error"><?php echo $phoneError; ?></span>
-            <input type="text" class="contact__form__phone" name="phone" placeholder="Telefon" value="<?= $phone ?>">
+            <input type="text" class="contact__form__phone" name="phone" placeholder="Telefon" value="<?=$phone;?>">
             <span class="error"><?php echo $messageError; ?></span>
             <textarea id="msg" cols="30" rows="15" name="msg" class="contact__form__msg" placeholder="Meddelande"></textarea>
             <input type="submit" class="contact__form__button" name="submit">
@@ -54,11 +50,11 @@ include __DIR__ . '/php/model/phpmailer.php';
     <script type="text/javascript"> shopLib.drawCategorySelectors(); </script>
 
     <script type="text/javascript" src="./js/cart.js"></script>
-    
+
     <script type="text/javascript">
-    document.getElementById("msg").value = "<?php echo $message ?>";
+    document.getElementById("msg").value = "<?php echo $message; ?>";
     </script>
-    
+
 </body>
 
 </html>
