90c0b5517448db5e2033338ac1c852a9fa1f29a3
Dmitrijv <dmitrij.vel@live.se>
2020-04-24 18:21:25 +0200

    switched to ie11 js on index.php

--- index.php
+++ index.php
@@ -34,8 +34,8 @@
     <script src="./js/ie11/sidebar.js"></script>
     <!-- <script src="./js/sidebar.js"></script> -->
 
-    <!-- <script type="text/javascript" src="./js/ie11/shopLib.js"></script> -->
-    <script type="text/javascript" src="./js/shopLib.js"></script>
+    <script type="text/javascript" src="./js/ie11/shopLib.js"></script>
+    <!-- <script type="text/javascript" src="./js/shopLib.js"></script> -->
     <script type="text/javascript">
         shopLib.drawCategorySelectors();
     </script>
