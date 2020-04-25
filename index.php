85606529e2082d3c0662febc7f46f789d0a5139b
Dmitrijv <dmitrij.vel@live.se>
2020-04-25 13:15:34 +0200

    removed unused code from sidebar.js

--- index.php
+++ index.php
@@ -32,7 +32,6 @@
     <?php require_once __DIR__ . '/php/view/footer.php';?>
 
     <script src="./js/ie11/sidebar.js"></script>
-    <!-- <script src="./js/sidebar.js"></script> -->
 
     <script type="text/javascript" src="./js/ie11/shopLib.js"></script>
     <!-- <script type="text/javascript" src="./js/shopLib.js"></script> -->
