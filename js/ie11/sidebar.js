5f7df2df0010365547561d313abfff86c76b8397
Dmitrijv <dmitrij.vel@live.se>
2020-04-25 13:20:31 +0200

    refactored sidebar.js to be ie11 compatible

--- js/ie11/sidebar.js
+++ js/ie11/sidebar.js
@@ -1,7 +1,7 @@
 (function () {
   const btn = document.querySelector(".hamburger__bar-wrapper");
   const sidebar = document.querySelector(".sidebar");
-  btn.onclick = () => {
+  btn.onclick = function () {
     sidebar.classList.toggle("active");
     btn.classList.toggle("active");
   };
