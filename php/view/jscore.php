6f46307fdd1993e192c7159f866c790a80ba65e2
g-holmer <gabbeholmer@hotmail.com>
2020-04-26 21:31:48 +0200

    fixed bugg with cart in latest and last chance

--- php/view/jscore.php
+++ php/view/jscore.php
@@ -1,12 +1,11 @@
 
     <script src="./js/ie11/sidebar.js"></script>
 
-    <script type="text/javascript" src="./js/ie11/cart.js"></script>
-    <!-- <script type="text/javascript" src="./js/cart.js"></script> -->
+    <!-- <script type="text/javascript" src="./js/ie11/cart.js"></script> -->
 
-    <script type="text/javascript" src="./js/ie11/shopLib.js"></script>
-    <!-- <script type="text/javascript" src="./js/shopLib.js"></script> -->
+    <!-- <script type="text/javascript" src="./js/ie11/shopLib.js"></script> -->
+    <script type="text/javascript" src="./js/shopLib.js"></script>
     <script type="text/javascript"> shopLib.drawCategorySelectors(); </script>
 
-
     <script type="text/javascript" src="./js/ie11/cartOverlay.js"></script>
+    <script type="text/javascript" src="./js/cart.js"></script>
