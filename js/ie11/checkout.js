cdaf5212135f0bd455e92e7f90130ef65c634b6b
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-04-27 16:36:32 +0200

    draw order table from localStorage, price needs to be checked again, have problem atm

--- js/ie11/checkout.js
+++ js/ie11/checkout.js
@@ -264,3 +264,43 @@ document.querySelector('.checkout-form__delivery-section__checkoutBtn--dim').onc
     event.preventDefault();
   }
 }
+
+
+/* Generate order list */
+var listArea = document.querySelector('.checkout-form__cart-section__product-list');
+
+var shoppingCart = JSON.parse(localStorage.getItem("products"));
+
+var subTotal = 0;
+var itemsCountTotal = 0;
+var orderList = document.querySelector(".order-list");
+var totalPrice = document.querySelector(".totalPrice");
+var productsAmount = document.querySelector(".products-amount");
+/* object structure
+        id
+        name
+        img
+        price
+        qty
+   */
+var length = shoppingCart.length;
+for (var a = 0; a < length; a++) {
+  var item = shoppingCart[a];
+  var itemName = item.name;
+  var itemCount = item.qty * 1;
+  var itemPrice = item.price.slice(0, -3);
+  var itemImage = item.img;
+  var itemTotalPrice = Math.ceil((1 * itemCount) * (1 * itemPrice));
+  subTotal += itemTotalPrice;
+  itemsCountTotal += itemCount;
+  orderList.innerHTML += "<tr><td class=\"item-image\"><img class=\"product-cover-small\" src=\"".concat(itemImage, "\" alt=\"").concat(itemName, "\"></td><td class=\"item-name\">  ").concat(itemName, "</td><td class=\"item-qty\">").concat(itemCount, "</td><td class=\"item-price\">").concat(itemPrice, "</td><td class=\"item-total\">").concat(itemTotalPrice, " kr</td></tr>");
+
+}
+
+subTotal = subTotal; //remove toFixed(2)
+
+// orderList.innerHTML += "<tr class=\"font-bold\"><td>Totalt:</td><td></td><td class=\"products-amount\">".concat(itemsCountTotal, "</td><td></td><td class=\"item-total\" >").concat(subTotal, " kr</td></tr></tbody>");
+
+var text = itemsCountTotal > 1 ? 'Artiklar' : 'Artikel';
+productsAmount.textContent = itemsCountTotal + ' ' + text;
+totalPrice.innerHTML = subTotal + ' kr';
