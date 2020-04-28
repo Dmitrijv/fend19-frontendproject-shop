5617c6ddef9271a3d8cdb00ad9af676a4e16ec40
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-04-28 16:29:06 +0200

    seperate click fun out

--- js/ie11/cartOverlay.js
+++ js/ie11/cartOverlay.js
@@ -3,24 +3,24 @@ var body = document.body,
   overlayBtts = document.querySelectorAll('button[class$="overlay"]');
 
 [].forEach.call(overlayBtts, function (btt) {
-
-  btt.addEventListener('click', function () {
-
-    /* Detect the button class name */
-    var overlayOpen = this.className === 'open-overlay';
-
-    /* Toggle the aria-hidden state on the overlay and the 
-       no-scroll class on the body */
-    overlay.setAttribute('aria-hidden', !overlayOpen);
-    body.classList.toggle('noscroll', overlayOpen);
-
-    /* On some mobile browser when the overlay was previously
-       opened and scrolled, if you open it again it doesn't 
-       reset its scrollTop property */
-    overlay.scrollTop = 0;
-
-  }, false);
+  btt.addEventListener('click', openCart, false);
 });
+
+//seperate function out
+function openCart() {
+  /* Detect the button class name */
+  var overlayOpen = this.className === 'open-overlay';
+
+  /* Toggle the aria-hidden state on the overlay and the 
+     no-scroll class on the body */
+  overlay.setAttribute('aria-hidden', !overlayOpen);
+  body.classList.toggle('noscroll', overlayOpen);
+
+  /* On some mobile browser when the overlay was previously
+     opened and scrolled, if you open it again it doesn't 
+     reset its scrollTop property */
+  overlay.scrollTop = 0;
+}
 
 // var activeOverlay = document.querySelector(['aria-hidden' == 'false']);
 // overlay.addEventListener('click', function () {
