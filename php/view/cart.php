a6de358b33d565cae5a467322f217e802e4a28bd
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-05-02 13:58:09 +0200

    cart style changes

--- php/view/cart.php
+++ php/view/cart.php
@@ -1,6 +1,6 @@
 <section class="overlay" aria-hidden="true">
     <div class="cart">
-        <button class="cart-close-btn close-overlay">X</button>
+        <button class="cart-close-btn close-overlay"><img src="./img/svg/close-red.svg"></button>
         <h2><span class='total-sum'></span></h2>
         <span class="cart__erase"></span>
         <div id="cart__product-list" class="cart__product-list"></div>
